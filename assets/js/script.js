c3c01f3c63ad5a9e6781d70c60eed7528459f0da
benbarker04 <>
2023-09-23 09:58:29 +0000

    set up structures of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+function runGame(){
+
+}
+
+function checkAnswer(){
+
+}
+
+function calculateCorrectAnswer(){
+
+}
+
+function incrementScore(){
+
+}
+
+function incrementWrongAnswers(){
+
+}
+
+function displayAdditionQuestion(){
+
+}
+
+function displaySubtractQuestion(){
+
+}
+
+function displayMultiplyQuestion(){
+
+}
+
