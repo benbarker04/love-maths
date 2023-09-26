df8b6d789e31becc78fc671be362d4b8ece1d96f
benbarker04 <>
2023-09-26 10:38:49 +0000

    Add favicon, and final tidy ups for the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,6 +12,12 @@ document.addEventListener('DOMContentLoaded', function(){
     })
   }
 
+  document.getElementById('answer-box').addEventListener('keydown', function(event){
+    if(event.key === 'enter'){
+        checkAnswer();
+    }
+  })
+
   runGame('addition');
 
 })
@@ -20,6 +26,8 @@ function runGame(gameType){
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
 
+    document.getElementById('answer-box').value = ''
+    document.getElementById('answer-box').focus();
 
     if(gameType === 'addition'){
         displayAdditionQuestion(num1, num2)
