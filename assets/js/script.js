20febc0d0255278cb4f3ad6769f7942fe3c4daa5
benbarker04 <>
2023-09-26 09:55:06 +0000

    Add code to icrement socre and incorrect answers tally

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,8 +36,10 @@ function checkAnswer(){
     
     if(isCorrect){
         alert('Hey! You got it right! :D')
+        incrementScore()
     }else{
         alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`)
+        incrementWrongAnswers()
     }
 
     runGame(calculatedAnswer[1]);
@@ -59,11 +61,13 @@ function calculateCorrectAnswer(){
 }
 
 function incrementScore(){
-
+    let oldScore = parseInt(document.getElementById('score').innerText);
+    document.getElementById('score').innerText = ++oldScore;
 }
 
 function incrementWrongAnswers(){
-
+    let oldScore = parseInt(document.getElementById('incorrect').innerText);
+    document.getElementById('incorrect').innerText = ++oldScore;
 }
 
 function displayAdditionQuestion(operand1, operand2){
